31e037c8631482228809d1189065ee1ecdf4a9d4
Phoebe <meipbe@gmail.com>
2017-11-08 20:25:17 -0800

    Completed server post request handler

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const db = require('../database/index.js')
+const github = require('../helpers/github.js')
 let app = express();
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+  let userName = req.body.query;
+  github.getReposByUsername(userName)
+    .then(results => {
+      db.save(results.body)
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.get('/repos', function (req, res) {
+  db.grabRepos().then((repos) => {
+    return repos;
+  })
   // TODO - your code here!
   // This route should send back the top 25 repos
 });
@@ -20,4 +32,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
