1369082e5937800f10d55ad5cebe1244ae3c3552
Phoebe <meipbe@gmail.com>
2017-11-08 22:36:55 -0800

    Added repo sorting method

--- helpers/github.js
+++ helpers/github.js
@@ -2,11 +2,6 @@ const request = require('request');
 const config = require('../config.js');
 
 let getReposByUsername = (username) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out,
-  // but you'll have to fill in the URL
   let options = {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
@@ -24,7 +19,6 @@ let getReposByUsername = (username) => {
       }
     });
   });
-
 }
 
 module.exports.getReposByUsername = getReposByUsername;
