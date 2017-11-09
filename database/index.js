3af466667f8d0022e1c3039a941abf42a695ecea
Phoebe <meipbe@gmail.com>
2017-11-08 20:21:23 -0800

    Added database save and fetch functions for repo entries

--- database/index.js
+++ database/index.js
@@ -1,16 +1,51 @@
 const mongoose = require('mongoose');
+const Promise = require("bluebird");
 mongoose.connect('mongodb://localhost/fetcher');
 
+mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+mongoose.connection.once('open', function() {
+  console.log('helloworld');
+})
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  author: String,
+  avatar: String,
+  description: String,
+  repoUrl: String,
+  forks_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let createNewEntry = (repo) => {
+  let newRepoProperties = {
+    author: repo.owner.login,
+    avatar: repo.owner.avatar_url,
+    repoName: repo.name,
+    description: repo.description,
+    url: repo.html_url,
+    forkCount: repo.forks_count
+  }
+  let newRepo = new Repo(newRepoProperties);
+  return newRepo.save();
+}
+
+let save = (results) => {
+  results = JSON.parse(results);
+  return Promise.map(results, function(repo) {
+    return createNewEntry(repo);
+  })
+}
+
+let grabRepos = () => {
+  return Repo.find(function(err, repos) {
+    if (err) {
+      console.log('Cannot retrive repos from database: ', err);
+    } else {
+      return repos;
+    }
+  })
 }
 
 module.exports.save = save;
+module.exports.grabRepos = grabRepos;
